5fc90ef6a6f7e2e09d3de140529a5bc5a10a89e9
akiladperera <akilamatrix@gmail.com>
2018-10-17 18:23:06 +0530

    new

--- python/client.py
+++ python/client.py
@@ -2,12 +2,20 @@
 
 import socket
 
+class Address():
+    self.ip = ""
+    self.port = ""
+
+    def __init__(self, ip, port):
+        
+
+
 HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 60000        # The port used by the server
+PORT = 65000        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'0036 REG 127.0.0.10 80001 akila')
+    s.sendall(b'0036 REG 127.0.0.10 8003 naveen')
     data = s.recv(1024)
 
 print(data.decode())
