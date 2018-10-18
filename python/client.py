3cc5135d378a2f24625ffd378acfb2a1b9fc79df
akiladperera <akilamatrix@gmail.com>
2018-10-18 22:28:00 +0530

    done

--- python/client.py
+++ python/client.py
@@ -8,7 +8,6 @@ import string
 import sys
 import time
 
-
 class Address():
     def __init__(self, ip, port, username=""):
         self.ip = ip
@@ -16,44 +15,48 @@ class Address():
         self.username = username
 
     def __eq__(self, other):
-        return self.ip == other.ip and self.port == other.port and self.username == other.username
+        return (self.ip==other.ip) and (self.port==other.port)
 
     def __repr__(self):
-        return "%s %d" % (self.ip, self.port)
-
+        return "%s %d %s" % (self.ip, self.port, self.username)
 
 class Server(threading.Thread):
-    buffer_size = 2048
-
     def __init__(self, address):
         threading.Thread.__init__(self)
         self.ip = address.ip
         self.port = address.port
 
-    def decodeMessage(self, message):
-        res = message.split()
-        if res[1].lower() == 'give':
-            address = Address(res[2], int(res[3]), res[4])
-            addNewNode(address)
-            if len(nodes) > 0:
-                if len(nodes) == 1:
-                    msg = "TAKE 1 %s %d %s" % (nodes[0].ip, nodes[0].port, nodes[0].username)
-                    print(msg)
-                    return msg
-                else:
-                    first = random.randint(0, len(nodes) - 1)
-                    second = -1;
-                    while True:
-                        second = random.randint(0, len(nodes) - 1)
-                        if second != first:
-                            break
-                    msg = "TAKE 2 %s %d %s %s %d %s" % (
-                    nodes[first].ip, nodes[first].port, nodes[first].username, nodes[second].ip, nodes[second].port,
-                    nodes[second].username)
-                    print(msg)
-                    return msg
-        else:
-            return "Hi"
+    def decode_give_request(self, req):
+        req = req.split()
+
+        num_char = int(req.pop(0))
+        command = req.pop(0)
+
+        addresses_ip = req.pop(0)
+        addresses_port = int(req.pop(0))
+        addresses_username = req.pop(0)
+
+        addr = Address(addresses_ip, addresses_port, addresses_username)
+
+        if not(addr in nodes):
+            nodes.append(addr)
+        
+        return addr
+
+    def compose_take_response(self, sender):
+        addresses = nodes[:] # make a copy
+        if (sender in addresses): addresses.remove(sender) # remove sender from the list
+
+        if len(addresses)==0:
+            return "ACTIVEOK"
+        elif len(addresses)==1:
+            return "TAKE 1 %s %d %s"%(addresses[0].ip, addresses[0].port, addresses[0].username)
+        elif len(addresses)>1:
+            add1 = random.choice(addresses)
+            add2 = random.choice(addresses)
+            while add1==add2:
+                add2 = random.choice(addresses)
+            return "TAKE 2 %s %d %s %s %d %s"%(add1.ip, add1.port, add1.username, add2.ip, add2.port, add2.username)
 
     def run(self):
         print("Starting client-side server...")
@@ -66,11 +69,76 @@ class Server(threading.Thread):
                 incoming_address = address[0]
                 incoming_port = int(address[1])
 
-                print("Message received: '%s' \t Address received: %s:%d" % (
-                    incoming_msg, incoming_address, incoming_port))
+                req = incoming_msg.split()
+                req_length = int(req[0])
+                req_command = req[1]
 
-                server.sendto(attach_length(self.decodeMessage(incoming_msg)).encode(), address)
+                if req_command=="GIVE":
+                    sender_addr = self.decode_give_request(incoming_msg)
+                    take_res = self.compose_take_response(sender_addr)
+                    if take_res!=None:
+                        server.sendto(attach_length(take_res).encode(), address)
 
+class Gossiping(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def request_addresses(self): # GIVE Request
+        if len(nodes)>0:
+            to = random.choice(nodes)
+            req = "GIVE %s %d %s" % (my_ip, my_port, my_name)
+
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as connection:
+                connection.settimeout(3)
+                for shy in range(2):
+                    try:
+                        connection.sendto(attach_length(req).encode(), (to.ip, to.port))
+                        res, address = connection.recvfrom(buffer_size)
+                        return res.decode()
+                        break
+                    except:
+                        pass
+                else:
+                    unreg(to)
+
+    def update_nodes(self, res_of_give):
+        res = res_of_give.split()
+
+        num_char = int(res.pop(0))
+        status = res.pop(0)
+        num_addresses = int(res.pop(0))
+
+        addr = []
+        for address_no in range(num_addresses):
+            ip = res.pop(0)
+            port = int(res.pop(0))
+            username = res.pop(0)
+            addr.append(Address(ip, port, username))
+        
+        for add in addr:
+            if not(add in nodes):
+                nodes.append(add)
+
+    def run(self):
+        print("Gossiping solution start...")
+        while True:
+            if len(nodes) >= node_limit:
+                # isactive request should be here
+                time.sleep(6)
+            elif len(nodes) < node_limit:
+                res = self.request_addresses() # response = None | ACTIVEOK | IPs
+                if (res!=None):
+                    if res.split()[1]!="ACTIVEOK":
+                        self.update_nodes(res)
+                time.sleep(3)
+
+def unreg(address):
+    # Unregister from boostrap
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        reg_msg = "UNREG %s %d %s" % (address.ip, address.port, address.username)
+        s.sendall(attach_length(reg_msg).encode())
+    nodes.remove(address)
 
 def decode_reg_response(response):
     res = response.split()
@@ -104,7 +172,6 @@ def decode_reg_response(response):
 
         return 1, addresses
 
-
 def get_available_port(ip):
     init_port = 6000
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -119,50 +186,15 @@ def get_available_port(ip):
             init_port += 1
     return init_port
 
-
 def attach_length(message):
     length = len(message) + 5
     return "%04d %s" % (length, message)
-
-
-my_ip = netifaces.ifaddresses('wlp3s0')[netifaces.AF_INET][0]['addr']  # you need to change eth0 accordingly.
-my_port = get_available_port(my_ip)
-my_name = "".join([random.choice(string.ascii_letters) for i in range(5)])
-my_address = Address(my_ip, my_port, my_name)
-
-# Boostrap server config
-HOST = my_ip
-PORT = 65000
-if len(sys.argv) == 3:
-    # port and ip given
-    try:
-        PORT = int(sys.argv[1])
-        HOST = sys.argv[2].strip()
-    except:
-        print("Error in input parameters")
-        exit(0)
-elif len(sys.argv) == 2:
-    # port given
-    try:
-        PORT = int(sys.argv[1])
-
-    except:
-        print("Error in input parameters")
-        exit(0)
-
-nodes = []
-
-buffer_size = 2048
-
-nodeLimit = 3
-
 
 def main():
     global nodes
 
     # Let's start the listening socket
-    server_thread = Server(my_address)
-    server_thread.start()
+    server_thread = Server(my_address).start()
 
     # Registration with bootstrap
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -186,43 +218,19 @@ def main():
                 nodes = addresses
     # Registration with bootstrap done
 
-    gosip = Gather()
-    gosip.start()
+    # Let's start the gossiping
+    gossiping_thread = Gossiping().start()
 
     print("continue main thread...")
 
     while True:
         query()
 
-
-def unreg(address):
-    # Unregister from boostrap
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        reg_msg = "UNREG %s %d %s" % (address.ip, address.port, address.username)
-        s.sendall(attach_length(reg_msg).encode())
-
-
-def sendMessage(msg, address, retFun):
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as connection:
-        connection.settimeout(3)
-        for shy in range(2):
-            try:
-                connection.sendto(attach_length(msg).encode(), (address.ip, address.port))
-                res, address = connection.recvfrom(buffer_size)
-                retFun(res.decode())
-                break
-            except:
-                print(shy)
-                pass
-        else:
-            unreg(address)
-            nodes.remove(address)
-
-
 def show_neighbours():
     print(nodes)
 
+def show_me():
+    print("My Details: %s %d %s" % (my_ip, my_port, my_name))
 
 def hi(neighbour):
     # Say hi to node
@@ -230,16 +238,13 @@ def hi(neighbour):
         req = "Hi, I am %s. What is name?" % (my_address.username)
         connection.sendto(req.encode(), (neighbour.ip, neighbour.port))
 
-
 def query():
     command = input("Enter your command: ").strip().lower()
 
-    if command == "show":
+    if command=="show":
         show_neighbours()
-
-    elif command == "my":
-        print("My Ip: %s %d %s" % (my_ip, my_port, my_name))
-
+    elif command=="my":
+        show_me()
     elif command.startswith("hi"):
         cmd_list = command.split()
         try:
@@ -250,49 +255,34 @@ def query():
         except:
             print("Wrong syntax...")
 
+my_ip = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']  # you need to change eth0 accordingly.
+my_port = get_available_port(my_ip)
+my_name = "".join([random.choice(string.ascii_letters) for i in range(5)])
+my_address = Address(my_ip, my_port, my_name)
 
-def addNewNode(address):
-    exist = False
-    for node in nodes:
-        if node.ip == address.ip and node.port == address.port:
-            exist = True
-            break
-    if (not exist) and address != my_address:
-        nodes.append(address)
+# Boostrap server config
+HOST = my_ip
+PORT = 65000
+if len(sys.argv) == 3:
+    # port and ip given
+    try:
+        PORT = int(sys.argv[1])
+        HOST = sys.argv[2].strip()
+    except:
+        print("Error in input parameters")
+        exit(0)
+elif len(sys.argv) == 2:
+    # port given
+    try:
+        PORT = int(sys.argv[1])
+    except:
+        print("Error in input parameters")
+        exit(0)
 
+nodes = []
 
-def takeIPsOfPeer(msgRet):
-    print("msg recieved: %s" % (msgRet))
-    res = msgRet.strip().split()
-    if res[1].lower() == 'take':
-        print("ABCcccccccccccccccccccccccccc")
-        peerCount = int(res[2])
-        if peerCount == 2:
-            print("Count 2: %s %s %s | %s %s %s" % (res[3], res[4], res[5], res[6], res[7], res[8]))
-            addNewNode(Address(res[3], int(res[4]), res[5]))
-            addNewNode(Address(res[6], int(res[7]), res[8]))
-        elif peerCount == 1:
-            print("Count 1: %s %s %s" % (res[3], res[4], res[5]))
-            addNewNode(Address(res[3], int(res[4]), res[5]))
+buffer_size = 2048
 
-
-class Gather(threading.Thread):
-    buffer_size = 2048
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def run(self):
-        print("Gossiping solution start...")
-        while True:
-            if len(nodes) >= nodeLimit:
-                print("gossip end triggered")
-                break
-            if len(nodes) > 0:
-                address = nodes[random.randint(0, len(nodes) - 1)]
-                msg = "GIVE %s %d %s" % (my_ip, my_port, my_name)
-                sendMessage(msg, address, takeIPsOfPeer)
-            time.sleep(3)
-
+node_limit = 3
 
 main()
