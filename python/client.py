c3343b3e396c176d885a593c739eccb1ad234ada
Akalanka <duminduakalanka@gmail.com>
2018-11-26 10:41:45 +0530

    passed a value from restapi search route

--- python/client.py
+++ python/client.py
@@ -551,7 +551,7 @@ while len(files)<file_count:
 file_source = "./files/"
 download_loc = "./download/"
 
-my_ip = netifaces.ifaddresses('enp0s20u1')[netifaces.AF_INET][0]['addr']  # you need to change eth0 accordingly.
+my_ip = netifaces.ifaddresses('wlp9s0')[netifaces.AF_INET][0]['addr']  # you need to change eth0 accordingly.
 my_port = get_available_port(my_ip, 6000)
 my_file_server_port = get_available_tcp_port(my_ip, 9500)
 my_web_server_port = get_available_tcp_port(my_ip, 9000)
