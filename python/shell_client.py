f841cf5fa7cabf2eddc58ed778b2892182b5f8a9
ErangaD <erangadulshan.14@cse.mrt.ac.lk>
2018-11-29 23:24:55 +0530

    added shell client

--- python/shell_client.py
+++ python/shell_client.py
@@ -29,15 +29,13 @@ class LinkTest(threading.Thread):
     def is_link_connected(self, address):
         req = "ACTIVE %s %d %s" % (address.ip, address.port, address.username)
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as connection:
-            connection.settimeout(1)
-            for shy in range(2):
-                try:
-                    connection.sendto(attach_length(req).encode(), (address.ip, address.port))
-                    res, address = connection.recvfrom(buffer_size)
-                    return True
-                except:
-                    pass
-            else:
+            connection.settimeout(40)
+            try:
+                connection.sendto(attach_length(req).encode(), (address.ip, address.port))
+                res, address = connection.recvfrom(buffer_size)
+                return True
+            except:
+                print("Error is link connected\n")
                 return False
 
     def run(self):
@@ -231,16 +229,15 @@ class Gossiping(threading.Thread):
         address = random.choice(nodes)
         req = "ACTIVE %s %d %s" % (address.ip, address.port, address.username)
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as connection:
-            connection.settimeout(1)
-            for shy in range(2):
-                try:
-                    connection.sendto(attach_length(req).encode(), (address.ip, address.port))
-                    res, address = connection.recvfrom(buffer_size)
-                    break
-                except:
-                    pass
-            else:
+            connection.settimeout(40)
+            try:
+                connection.sendto(attach_length(req).encode(), (address.ip, address.port))
+                res, address = connection.recvfrom(buffer_size)
+                break
+            except:
                 remove_from_nodes(address)
+                print("Gossiping Error is link connected\n")
+                
 
     def request_addresses(self): # GIVE Request
         if len(nodes)>0:
@@ -248,17 +245,14 @@ class Gossiping(threading.Thread):
             req = "GIVE %s %d %s" % (my_ip, my_port, my_name)
 
             with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as connection:
-                connection.settimeout(1)
-                for shy in range(2):
-                    try:
-                        connection.sendto(attach_length(req).encode(), (to.ip, to.port))
-                        res, address = connection.recvfrom(buffer_size)
-                        return res.decode()
-                        break
-                    except:
-                        pass
-                else:
+                connection.settimeout(40)
+                try:
+                    connection.sendto(attach_length(req).encode(), (to.ip, to.port))
+                    res, address = connection.recvfrom(buffer_size)
+                    return res.decode()
+                except:
                     remove_from_nodes(to)
+                    print("Gossiping Error request_address")
 
     def update_nodes(self, res_of_give):
         res = res_of_give.split()
@@ -283,14 +277,14 @@ class Gossiping(threading.Thread):
         while True:
             if len(nodes) >= node_limit:
                 self.is_link_connected()
-                time.sleep(5)
+                time.sleep(20)
                 
             elif len(nodes) < node_limit:
                 res = self.request_addresses() # response = None | ACTIVEOK | IPs
                 if (res!=None):
                     if res.split()[1]!="ACTIVEOK":
                         self.update_nodes(res)
-                time.sleep(5)
+                time.sleep(20)
             
             # Check kill switch
             if kill_switch==1: 
@@ -363,7 +357,7 @@ def remove_from_nodes(node_address):
     try:
         nodes.remove(node_address)
     except:
-        pass
+        print("Error remove_from_nodes")
 
 def unreg():
     # Unregister from boostrap
